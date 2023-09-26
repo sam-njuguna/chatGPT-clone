404157e0b622e3f2d1a9c65693d2a4153c2d75d0
sam-njuguna <sam.x.njuguna@gmail.com>
2023-09-26 22:58:20 +0300

    feat: modal

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,5 +1,3 @@
-import ThemeSwitcher from "./ThemeSwitcher";
 import ThemeProvider from "./ThemeProvider";
 import { ModalProvider } from "./ModalContext";
-export { ThemeSwitcher, ThemeProvider, ModalProvider };
-
+export { ThemeProvider, ModalProvider };
