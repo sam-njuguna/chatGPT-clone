5d8d7157fdee9c55c393bc66c74819b297c3fcea
sam-njuguna <sam.x.njuguna@gmail.com>
2023-09-29 02:37:50 +0300

    feat:done

--- src/components/CustomText.tsx
+++ src/components/CustomText.tsx
@@ -42,12 +42,12 @@ const CustomText: React.FC = () => {
     const shuffled = [...dataArray].sort(() => Math.random() - 0.5);
     setShuffledData(shuffled);
     setCurrentIndex(0);
-    setCurrentSubtitles(shuffled[0].subtitles);
+    setCurrentSubtitles(shuffled[0]?.subtitles || []);
 
     const timer = setInterval(() => {
       setCurrentIndex((prevIndex) => {
         const nextIndex = (prevIndex + 1) % shuffled.length;
-        setCurrentSubtitles(shuffled[nextIndex].subtitles);
+        setCurrentSubtitles(shuffled[nextIndex]?.subtitles || []);
         return nextIndex;
       });
     }, 10000);
@@ -62,12 +62,12 @@ const CustomText: React.FC = () => {
       {currentIndex !== -1 && (
         <div className="min-h-[100px] pt-[30px]">
           <h1 className="text-[32px] md:text-[40px] font-bold leading-[1.2]">
-            {shuffledData[currentIndex].title}
+            {shuffledData[currentIndex]?.title || ""}
           </h1>
           <TypingText
             textArray={currentSubtitles}
             delayBetweenTexts={10000}
-            cursorBlinkSpeed={500} // Cursor blink speed (0.5 seconds)
+            cursorBlinkSpeed={500}
           />
         </div>
       )}
