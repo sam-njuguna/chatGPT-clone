2830e68f84a8feadb0f357b1054f442969f14a8e
sam-njuguna <sam.x.njuguna@gmail.com>
2023-09-29 01:28:33 +0300

    feat:localstorage

--- src/components/modals/Customize.tsx
+++ src/components/modals/Customize.tsx
@@ -36,7 +36,7 @@ const Customize: FC<CustomizeProps> = ({}) => {
         className=" max-w-[576px] mx-auto pt-[6rem]  w-full max-xphone:px-2 max-md:px-4"
       >
         <div className="bg-white text-gray-700 dark:bg-home-color-nav_bg  w-full rounded-lg  dark:text-gray-300">
-          <div className="text-[18px] flex items-center gap-2 pt-6 pb-6 px-6 border-b border-white/10">
+          <div className="text-[18px] flex items-center gap-2 pt-6 pb-6 px-6 border-b  border-black/10 dark:border-white/10 ">
             <p className="font-semibold">Custom instructions</p>
             <TbInfoCircle size={18} />
           </div>
